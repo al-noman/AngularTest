dd859a571a242688dcd0f4804a94a5f8d80ad3c8
alnomaab <abdullah.alnoman@objego.de>
2021-11-08 12:45:40 +0100

    Finished until 'exploring the Angular template syntax'

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -4,25 +4,57 @@ import {Component, Input, Output, EventEmitter} from "@angular/core";
   selector: 'event-thumbnail',
   template: `
     <div class="well hoverwell thumbnail">
-      <h2>{{event.name}}</h2>
-      <div>Date: {{event.date}}</div>
-      <div>Time: {{event.time}}</div>
-      <div>Price: \${{event.price}}</div>
-      <div>
-        <span>Location: {{event.location.address}}</span>
-        <span>&nbsp;</span>
-        <span>{{event.location.city}}, {{event.location.country}}</span>
+      <h2 [style]="'font-style: italic;'">{{event?.name}}</h2>
+      <div>Date: {{event?.date}}</div>
+      <div [ngClass]="getStartTimeClass()"
+           [ngSwitch]="event?.time" >
+        Time: {{event?.time}}
+        <span *ngSwitchCase="'8:00 am'">(Early shift)</span>
+        <span *ngSwitchCase="'10:00 am'">(Late shift)</span>
+        <span *ngSwitchDefault>(Normal Shift)</span>
       </div>
-      <button class="btn btn-primary" (click)="handleClick()">Click Me</button>
+      <div [class.red]="event?.price > 900" [ngStyle]="getStyleForCheapPrice()">Price: \${{event?.price}}</div>
+      <div *ngIf="event?.location">
+        <span>Location: {{event?.location?.address}}</span>
+        <span class="pad-left">{{event?.location?.city}}, {{event?.location?.country}}</span>
+      </div>
+      <div [hidden]="!event?.onlineUrl">
+        Online URL: {{event?.onlineUrl}}
+      </div>
+<!--      <button class="btn btn-primary" (click)="handleClick()" style="display: none">Click Me</button>-->
     </div>
-  `
+  `,
+  styles: [`
+    .thumbnail {min-height: 250px;}
+    .pad-left {margin-left: 10px;}
+    .well div {color: #bbb;}
+    .green {color: green !important;}
+    .red {color: red !important;}
+    .bold {font-weight: bold; font-size: large;}
+  `],
 })
 export class EventThumbnailComponent {
-  @Input() event: any
+  @Input() event: any;
   @Output() childEvent = new EventEmitter();
 
   handleClick() {
-    console.log("Log from child");
+    console.log('Log from child');
     this.childEvent.emit(this.event);
+  }
+
+  logFoo() {
+    console.log('This is to test template variable');
+  }
+
+  getStartTimeClass() {
+    if (this.event.time === '10:00 am')
+      return 'red'
+    return ['green', 'bold']
+  }
+
+  getStyleForCheapPrice() {
+    if (this.event?.price < 700) {
+      return {color: 'green', 'font-size': 'xx-large'}
+    }
   }
 }
